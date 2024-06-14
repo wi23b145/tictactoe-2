ea7e71c3f12ed30e3b2208d47dbf3130dfab7829
wi23b069 <wi23b069@technikum-wien.at>
2024-06-14 15:42:31 +0200

    add us-1

---
+++ src/main/java/at/technikum/Board.java
@@ -0,0 +1,55 @@
+package at.technikum;
+
+public class Board {
+    private char[][] cells;
+
+    public Board() {
+        cells = new char[3][3];
+        clear();
+    }
+
+    public boolean isCellEmpty(int x, int y) {
+        if (x < 0 || x >= 3 || y < 0 || y >= 3) {
+            throw new IllegalArgumentException("Invalid cell coordinates");
+        }
+        return cells[x][y] == ' ';
+    }
+
+    public void place(int x, int y, char marker) {
+        if (x < 0 || x >= 3 || y < 0 || y >= 3) {
+            throw new IllegalArgumentException("Invalid cell coordinates");
+        }
+        if (!isCellEmpty(x, y)) {
+            throw new IllegalArgumentException("Cell is already occupied");
+        }
+        cells[x][y] = marker;
+    }
+
+    // rest einfügen
+
+
+    public void print() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(cells[i][j]);
+                if (j < 2) System.out.print(" | ");
+            }
+            System.out.println();
+            if (i < 2) System.out.println("---------");
+        }
+    }
+
+    public char[][] getCells() {
+        return cells;
+    }
+
+
+
+
+
+
+
+
+
+
+}
